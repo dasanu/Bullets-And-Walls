21fc7e5836defde202b7897c18a21ee17a44a82c
dasanu <73535623+dasanu@users.noreply.github.com>
2020-11-19 11:33:54 +0530

    Add files via upload

---
+++ bullet.js
@@ -0,0 +1,8 @@
+function hasCollided(Lbullet,Lwall){
+    bulletRightEdge=bullet.x+bullet.width;
+    wallLeftEdge=wall.x;
+    if(bulletRightEdge>=wallLeftEdge){
+      return true;
+    }
+    return false;
+ }
